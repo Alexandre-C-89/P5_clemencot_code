23095e807f2d41c7f769a464e2cfb1478bd79794
Alexandre <clemencot.alexandre@gmail.com>
2021-11-04 14:34:53 +0100

    Rajout urlSearchParams + insertion de l'id de la commande dans la balise orderId

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,12 +1,21 @@
 // ------------- variables utiles pour la suite du code --------------- //
 
-// const queryString = window.location.search;
-// const urlParams = new URLSearchParams(queryString);
-// const canapId = urlParams.get("id");
-// let urlAPI = "http://localhost:3000/api/products/"+ canapId;
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const idCommand = urlParams.get("id");
+let urlAPI = "http://localhost:3000/api/products/"+ idCommand;
 let orderId = document.getElementById("orderId");
 
 
+console.log(queryString);
+console.log(urlParams);
+console.log(idCommand);
 console.log(orderId);
+
+// ********************* //
+
+// ------------ J'insère l'id de la commande dans la balise avec l'id orderId ------------ //
+
+orderId.innerHTML = idCommand;
 
 // ********************* //
